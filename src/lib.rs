f7a298bc79c6fbd3192c0847933aef4ca75c28c4
Teo <teo.orthlieb@gmail.com>
2023-08-17 15:28:50 +0200

    added deref for signal

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
 mod noise;
 mod algebra;
 pub use crate::noise::NoiseSource;
-use std::ops::RangeInclusive;
+use std::ops::{RangeInclusive, Deref};
 
 #[derive(Clone)]
 pub struct Signal<N> {
     pub(crate) value: N,
     pub(crate) domain: RangeInclusive<f64>
+}
+
+impl<N> Deref for Signal<N> {
+    type Target = N;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
 }
 
 
