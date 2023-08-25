376240f31b8e1e695168e537be9693661586e4b0
Teo <teo.orthlieb@gmail.com>
2023-08-25 12:03:18 +0200

    fixed incorrect formula for mask

--- src/algebra/mask.rs
+++ src/algebra/mask.rs
@@ -3,6 +3,7 @@ use ndarray::Array1;
 
 impl Signal<Array1<f64>> {
     pub fn mask(self, t: f64) -> Self {
+        let t = 1. - 2.*t;
         Signal { 
             value: 1.0 / (1.0 + (-16. * (self.value - t)).map(|v| v.exp())), 
             domain: 0f64..=1f64
