f587e3c62285db9cc644f72048734bcc7ea22e37
Teo <teo.orthlieb@gmail.com>
2023-12-27 22:09:43 +0100

    more tweaking

--- src/algebra/threshold.rs
+++ src/algebra/threshold.rs
@@ -4,7 +4,7 @@ use ndarray::{Array, Dimension};
 impl<D: Dimension> Signal<Array<f32, D>> {
     pub fn threshold(self, t: f32) -> Self {
         Signal { 
-            value: 1.0 / (1.0 + (-32. * (self.value - t + 0.15)).map(|v| v.exp())), 
+            value: 1.0 / (1.0 + (-64. * (self.value - t + 0.2)).map(|v| v.exp())), 
             domain: 0f32..=1f32
         }
     }
