ca9cedc05f23be94ee02d3e7531be9d78d8285f1
Teo <teo.orthlieb@gmail.com>
2023-12-27 21:19:15 +0100

    fixed threshold + v bumb

--- src/algebra/threshold.rs
+++ src/algebra/threshold.rs
@@ -3,7 +3,6 @@ use ndarray::{Array, Dimension};
 
 impl<D: Dimension> Signal<Array<f32, D>> {
     pub fn threshold(self, t: f32) -> Self {
-        let t = 1. - 2.*t;
         Signal { 
             value: 1.0 / (1.0 + (-16. * (self.value - t)).map(|v| v.exp())), 
             domain: 0f32..=1f32
